9cc655570cf0328655bbf929ddad0d975c757610
NVIAM37 <nviam.037@gmail.com>
2026-01-09 01:52:17 +0530

    chore: remove fallback url since env var is set

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ connect();
 
 const app = express();
 
-const allowedOrigins = [process.env.FRONTEND_URL, 'http://localhost:5173', 'http://localhost:5174', 'https://nviam-code.vercel.app']
+const allowedOrigins = [process.env.FRONTEND_URL, 'http://localhost:5173', 'http://localhost:5174']
     .filter(Boolean)
     .map(url => url.replace(/\/$/, ''));
 
